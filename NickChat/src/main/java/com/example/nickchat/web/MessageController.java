038e043dab810611fef916ca6280c709719c1595
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-03-24 10:01:36 +0200

    Add files via upload

--- NickChat/src/main/java/com/example/nickchat/web/MessageController.java
+++ NickChat/src/main/java/com/example/nickchat/web/MessageController.java
@@ -1,13 +1,27 @@
 package com.example.nickchat.web;
 
+import com.example.nickchat.model.MessageModel;
+import com.example.nickchat.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageController {
-    
-    @MessageMapping
-    public void sendMessage() {
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, MessageModel message) {
+        System.out.println("Handling send message: " + message + " to: " + to );
+        boolean isExist = UserStorage.getInstance().getUsers().contains(to);
+
+        if (isExist){
+            simpMessagingTemplate.convertAndSend("/topic/messages" + to, message);
+        }
     }
 }
