3f6e0c31e24067c9181c5efb977c430145e417d6
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-03-24 09:16:57 +0200

    Add files via upload

---
+++ NickChat/src/main/java/com/example/nickchat/NickChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.nickchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NickChatApplication.class, args);
+    }
+
+}
---
+++ NickChat/src/test/java/com/example/nickchat/NickChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nickchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
