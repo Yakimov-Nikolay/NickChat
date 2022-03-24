1f7fd9dbeddab95c3e5553edb34b2154737996b1
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-03-24 09:45:07 +0200

    Add files via upload

--- NickChat/src/main/java/com/example/nickchat/storage/UserStorage.java
+++ NickChat/src/main/java/com/example/nickchat/storage/UserStorage.java
@@ -1,17 +1,31 @@
 package com.example.nickchat.storage;
 
+import java.util.Set;
+
 public class UserStorage {
 
     private static UserStorage instance;
+    private Set<String> users;
 
-    private UserStorage(){
+    private UserStorage() {
 
     }
 
-    public static synchronized UserStorage getInstance(){
-        if(instance == null){
+    public static synchronized UserStorage getInstance() {
+        if (instance == null) {
             instance = new UserStorage();
         }
         return instance;
+    }
+
+    private Set<String> getUsers() {
+        return users;
+    }
+
+    private void setUsers(String username) throws Exception {
+        if (users.contains(username)) {
+            throw new Exception("User already exist whit username: " + username);
+        }
+        users.add(username);
     }
 }
